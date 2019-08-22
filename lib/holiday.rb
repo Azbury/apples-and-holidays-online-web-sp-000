e0f6a70518977a48167287ef496b2fc67791f60e
Austin Asbury <acasbury25@aol.com>
2019-08-22 01:43:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
-    puts "#{holiday}"
+    puts "#{holiday.to_s.capitalize!}"
     holiday.each_with_index do |supply, i|
       if i == holiday.length - 1
         puts "#{supply}"
